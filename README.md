aaa55247e4c5b04abc3c45f4d7adf7c122b821d4
Juan P <pront23@gmail.com>
2018-09-17 09:33:35 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# pokemon_api_app
+
+A new Flutter application for Pokemon.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+import 'dart:convert'; //needed to convert the JSON data
+
+import 'package:pokemon_api_app/pokemon.dart';
+import 'package:pokemon_api_app/pokemon_details.dart';
+
+
+
+void main() => runApp(MaterialApp(
+  title: "Pokemon App",
+  home: HomePage(),
+  debugShowCheckedModeBanner: false,
+));
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() {
+    return new HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+
+  var url = 'https://raw.githubusercontent.com/Biuni/PokemonGo-Pokedex/master/pokedex.json';
+
+  PokeHub pokeHub;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchData();
+    //print('2nd task');
+  }
+  fetchData() async {
+    var resp = await http.get(url);
+    print(resp.body);
+    var decodedJson = jsonDecode(resp.body);
+
+    pokeHub = PokeHub.fromJson(decodedJson);
+    print(pokeHub.toJson());
+    setState(() {
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pokemon App"),
+        backgroundColor: Colors.pink,
+      ),
+      body: pokeHub == null
+          ? Center(child: CircularProgressIndicator(),
+      ) : GridView.count(crossAxisCount: 2,
+        children: pokeHub.pokemon.map((poke) => Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: InkWell(
+            onTap: (){
+              Navigator.push(
+                  context, MaterialPageRoute(
+                  builder: (context) => PokeDetails(
+                pokemon: poke,
+              )));
+            },
+            child: Hero(
+              tag: poke.img,
+              child: Card(
+                elevation: 3.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(poke.img))
+                      ),
+                    ),
+                    Text(poke.name,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight:
+                        FontWeight.bold ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        )).toList(),
+      ),
+      drawer: Drawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        backgroundColor: Colors.pinkAccent,
+      child: Icon(Icons.refresh),
+      ),
+    );
+  }
+}
+
+
